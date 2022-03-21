71030a181f881a24f4ead70777f35aab256f2a7f
wangwenjie <1286271989@qq.com>
2022-03-21 17:12:28 +0800

    完善表关系

--- api/main.go
+++ api/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"log"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -24,11 +23,10 @@ type App struct {
 	DummyNodes       []DummyNode `json:"dummy_nodes"`
 }
 
-//通过这样手动指定表明 通常
-
-//func(o App) TableName()string{
-//	return "app"
-//}
+func (o App) TableName() string {
+	//todo 补充表名
+	return ""
+}
 
 //AppProxyDummyNode 中间件表  举例client_token为 111 的用户拥有 tx1 分组的 proxy 和 dn1 的 dummy_node
 type AppProxyDummyNode struct {
@@ -37,10 +35,14 @@ type AppProxyDummyNode struct {
 	DummyNodeTag string `json:"dummy_node_tag"` //dummy_node_tag
 }
 
+func (o AppProxyDummyNode) TableName() string {
+	//todo 补充表名
+	return ""
+}
+
 // Proxy 节点表
 type Proxy struct {
-	Id int
-	//ProxyTag      string    `gorm:"primaryKey:ProxyTag;joinForeignKey:ProxyTag"" json:"proxy_tag"` // 指定外键
+	Id            int       `json:"id" gorm:"id"`
 	ProxyTag      string    `gorm:"proxy_tag" json:"proxy_tag"`             // 指定外键
 	Network       string    `json:"network" gorm:"network"`                 // 节点的类型，tcp/kcp
 	Addr          string    `json:"addr" gorm:"addr"`                       // 节点的外网地址
@@ -48,13 +50,22 @@ type Proxy struct {
 	AppUpdateTime time.Time `json:"app_update_time" gorm:"app_update_time"` // Apps最后更新的时间
 }
 
+func (o Proxy) TableName() string {
+	//todo 补充表名
+	return ""
+}
+
 // DummyNode 虚拟节点表
 type DummyNode struct {
-	Id int
-	//DummyNodeTag string `gorm:"primaryKey:DummyNodeTag;joinForeignKey:DummyNodeTag" json:"dummy_node_tag"` // 指定外键
+	Id           int    `json:"id" gorm:"id"`
 	DummyNodeTag string `gorm:"dummy_node_tag" json:"dummy_node_tag"` // 指定外键
-	Network      string `json:"network"`                              // 节点的类型，tcp/kcp/quic
-	Addr         string `json:"addr"`                                 // 节点的外网地址
+	Network      string `json:"network" gorm:"network"`               // 节点的类型，tcp/kcp/quic
+	Addr         string `json:"addr" gorm:"addr"`                     // 节点的外网地址
+}
+
+func (o DummyNode) TableName() string {
+	//todo 补充表名
+	return ""
 }
 
 var db *gorm.DB
@@ -63,7 +74,7 @@ var err error
 func main() {
 	db, err = gorm.Open("mysql", "root:123456.ab@/api?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
-		log.Fatal("db connect error")
+		panic(err)
 	}
 
 	db.AutoMigrate(&App{}, &Proxy{}, &DummyNode{}, &AppProxyDummyNode{})
@@ -73,21 +84,35 @@ func main() {
 		GatewayRules:     "80:80",
 		ServerAddr:       "192.168.1.1:8888",
 		ServerListenAddr: "0.0.0.0:8888",
-		ClientListenIP:   "127.0.0.2"}
-	db.Create(&u1)
+		ClientListenIP:   "127.0.0.2",
+	}
+	if e := db.Create(&u1).Error; e != nil {
+		panic(e)
+	}
+	mapping := AppProxyDummyNode{
+		ClientToken:  "111",
+		ProxyTag:     "tx1",
+		DummyNodeTag: "dn1",
+	}
+	if e := db.Model(mapping).Create(&mapping).Error; e != nil {
+		panic(e)
+	}
 	u2 := Proxy{
 		ProxyTag:      "tx1",
 		Network:       "tcp",
 		Addr:          "192.192.192.192:111",
 		ListenAddr:    "0.0.0.0:111",
 		AppUpdateTime: time.Now()}
-	db.Create(&u2)
+	if e := db.Create(&u2).Error; e != nil {
+		panic(e)
+	}
 	u3 := DummyNode{
 		DummyNodeTag: "dn1",
 		Network:      "tcp",
 		Addr:         "122.122.122.122"}
-	db.Create(&u3)
-
+	if e := db.Create(&u3).Error; e != nil {
+		panic(e)
+	}
 	defer db.Close() //延时调用函数
 
 	r := gin.Default()
@@ -131,32 +156,26 @@ func store(c *gin.Context) {
 func show1(c *gin.Context) {
 	CT := c.Params.ByName("client_token")
 	var app App
-	var apd AppProxyDummyNode
 	//get from app table
-	//这里没问题是因为,他默认表明就是小写加上 _ 的形式
-	//比如 UserName,你不手动指定,gorm 就认为是 user_name 表
-	if e := db.Model(App{}).Where("client_token=?", CT).First(&apd.).Related(&apd.ProxyTag).Error; e != nil {
+	if e := db.Model(App{}).Where("client_token=?", CT).First(&app).Error; e != nil {
 		panic(e)
 	}
 
-	//get from proxy table
-	//if e := db.Preload("proxies").Where("proxy_tag=?",apd.ProxyTag).Find(&apd).Error; e != nil {
-	//	panic(e)
-	//}
+	//get mapping related from middle table
+	var mapping AppProxyDummyNode
+	if e := db.Model(mapping).Where("client_token=?", CT).First(&mapping).Error; e != nil {
+		panic(e)
+	}
 
-	//get from dummy_node table
-	//se
-	//if e := db.Model(DummyNode{}).Where("dummy_node_tag=?", CT).Find(&app).Error; e != nil {
-	//	panic(e)
-	//}
+	//get record from proxy table
+	if e := db.Model(Proxy{}).Where("proxy_tag=?", mapping.ProxyTag).Find(&app).Error; e != nil {
+		panic(e)
+	}
 
-	////输入 client_token=11,输出 proxy dummy_node 表对应的数据
-	//
-	//fmt.Println("before related",jsonOutput(app))
-	//
-	//if e:=db.Model(app).Find(&app).Related(&app.Proxies, "proxies").Related(&app.DummyNodes, "dummy_nodes").Error;e!=nil{
-	//		panic(err)
-	//}
+	//get record from dummy_node table
+	if e := db.Model(DummyNode{}).Where("dummy_node_tag=?", mapping.DummyNodeTag).Find(&app).Error; e != nil {
+		panic(e)
+	}
 
 	if app.ClientToken == "" {
 		c.JSON(404, gin.H{"message": "user not found"})
